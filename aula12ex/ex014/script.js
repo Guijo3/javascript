a505f259008d7b648c1f2263a4a9fe0893715697
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-08-29 19:21:53 -0300

    exe JS BOM: dia, tarde e noite

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,25 @@
 //alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var dtn = window.document.getElementById('dtn')
+    var data = new Date()
+    var hora = 22 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora>= 5 && hora < 12) {
+        // BOM DIA !
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#d1ae76'
+        dtn.innerHTML = `<p><strong>BOM DIA !</strong></p>`
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE !
+        img.src = 'fototarde.png'
+        document.body.style.background = '#a36c58'
+        dtn.innerHTML = `<p><strong>BOA TARDE !</strong></p>`
+    } else {
+        // BOA NOITE !
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2b3849'
+        dtn.innerHTML = `<p><strong>BOA NOITE !</strong></p>`
+    }
+}
