7732f9ec5e0d9d1df4b85953272e9d5cae710acf
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-08-29 21:36:00 -0300

    exe JS idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -16,40 +16,61 @@ function verificar() {
             gênero = 'Homem'
             if (idade >=0 && idade <= 4) {
                 // BEBE
-                img.setAttribute('src', 'foto-bebehomi.png')
+                img.setAttribute('src', 'bebehomi.png')
             } else if (idade >= 5 && idade <= 12) {
                 // CRIANÇA
+                img.setAttribute('src', 'menino.png')
             } else if (idade >= 13 && idade <= 17) {
                 // ADOLECENTE
+                img.setAttribute('src', 'adolecentemasc.png')
             } else if (idade >= 18 && idade <= 25) {
                 // JOVEM
+                img.setAttribute('src', 'jovemhomi.png')
             } else if (idade >= 26 && idade <= 50) {
                 // HOMEM
+                img.setAttribute('src', 'homem.png')
             } else if (idade >= 51 && idade <= 65) {
                 // HOMEM MADURO
-            } else {
+                img.setAttribute('src', 'homemmaduro.png')
+            } else if (idade > 65 && idade <= 120){
                 // IDOSO
+                img.setAttribute('src', 'velho.png')
+            } else {
+                // MUMIA
+                img.setAttribute('src', 'mumia.png')
             }
 
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade <= 4) {
                 // BEBE
+                img.setAttribute('src', 'bebemuie.png')
             } else if (idade >= 5 && idade <= 12) {
                 // CRIANÇA
+                img.setAttribute('src', 'menina.png')
             } else if (idade >= 13 && idade <= 17) {
                 // ADOLECENTE
+                img.setAttribute('src', 'adolecentefem.png')
             } else if (idade >= 18 && idade <= 25) {
                 // JOVEM
+                img.setAttribute('src', 'jovemmuie.png')
             } else if (idade >= 26 && idade <= 50) {
                 // MULHER
+                img.setAttribute('src', 'mulher.png')
             } else if (idade >= 51 && idade <= 65) {
-                // MULHER MADURO
+                // MULHER MADURA
+                img.setAttribute('src', 'mulhermadura.png')
+            } else if (idade > 65 && idade <= 120){
+                // VELHA
+                img.setAttribute('src', 'velha.png')
             } else {
-                // IDOSA
+                // ESQUELETA
+                img.setAttribute('src', 'esqueleta.png')
             }
+
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)  // COMANDO PARA ADICIONAR UM ELEMENTO: (no caso) A IMAGEM DE RESULTADO 
     }
 }
