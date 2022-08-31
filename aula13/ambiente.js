a30d5133abca68d8b445632edffe17ccf71ccddd
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-08-31 19:30:59 -0300

    gg

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)       // ESTRUTURA DE REPETIÇÃO
+    c++  // c = c + 1               // TESTE LOGICO NO FIM
+ } while (c <= 6)
+
+/*var c = 1   
+while (c <= 6) {                       // ESTRUTURA DE REPETIÇÃO 
+    console.log(`Passo ${c}`)          // TESTE LOGICO NO INÍCIO
+    c++  // c = c + 1  
+}
+*/
+
+/*
+console.log('Tudo bem?')   // jeito manual de REPETIÇÃO
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
