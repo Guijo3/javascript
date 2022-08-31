d22c339182b0edf294581704aba4672d209310d7
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-08-31 20:40:42 -0300

    exe c/ repetições

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,7 @@
 //alert('Olá')
+function calcular() {
+    var inicio = window.document.querySelector('input#in1')
+    var fim = window.document.querySelector('input#in2')
+    var passo = window.document.querySelector('input#pn1')
+    var res = window.document.querySelector('div#res')
+}
