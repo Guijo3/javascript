99fb128f67362992aaca99a312053333357f5e79
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-09-02 17:58:38 -0300

    vetor

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+
+//num.push(1) // vai adicionar mais um conteudo no final do vetor. recomendado usar esse comando logo apos a declaração do vetor, em seguida o comando: nomedavariavel.sort() para organizar os conteudos
+//num.sort() // comando p/ mostrar os conteudos do vetor em ordem do < p/ o >
+//console.log(`o primeiro valor do vetor é ${num[0]}`) a contagem do vetor começa na posição: 0º, 1º, 2º, 3º,...etc
+//console.log(`O vetor tem ${num.length} posições`) p/ saber o comprimento do vetor
+//console.log(`Nosso vetor é o: ${num}`) //para mostrar o vetor inteiro
