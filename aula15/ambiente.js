5e727479b4bac88c80d06a5e93d2efa0ad5522c2
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-09-03 12:02:48 -0300

    detalhes sobre vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,5 +1,17 @@
 let num = [5, 8, 2, 9, 3]
 
+
+let pos = num.indexOf(8) // esse comando irá mostrar em qual indice o conteudo se encontra
+if (pos == -1) { // se o resultado der -1 quer dizer q o conteudo não foi encontrado
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
+
+
+
+
+
 //num.push(1) // vai adicionar mais um conteudo no final do vetor. recomendado usar esse comando logo apos a declaração do vetor, em seguida o comando: nomedavariavel.sort() para organizar os conteudos
 //num.sort() // comando p/ mostrar os conteudos do vetor em ordem do < p/ o >
 //console.log(`o primeiro valor do vetor é ${num[0]}`) a contagem do vetor começa na posição: 0º, 1º, 2º, 3º,...etc
---
+++ aula15/vetornatela.js
@@ -0,0 +1,34 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+valores.sort() // para arrumar a ordem dos conteudos do < para o >
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+} // jeito mais facil de mostrar o vetor e seus conteudos ordenadamente
+
+
+
+
+/*for(let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/  
+    // jeito monstro top demais de mostrar todas as posições
+   // referente ao conteudo do vetor
+
+
+
+
+
+
+//console.log(valores) jeito simples mais não detalhado de mostrar 
+//o vetor e seus conteudos
+
+
+/*
+console.log(valores[0])
+console.log(valores[1]) jeito menos inteligente de mostrar o vetor e seus conteudos
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+*/
