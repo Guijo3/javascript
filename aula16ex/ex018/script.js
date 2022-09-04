bcde4c847aeb039002dbd94ee9ed3f4d8a456e63
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2022-09-04 17:12:36 -0300

    atts

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,1 +1,21 @@
 //alert('Olá')
+let num = window.document.querySelector('input#txtn1')
+let lista = window.document.querySelector('select#flista')
+let res = window.document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+
+}
+
+function inLista(n, l) {
+
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        window.alert('Valor Inválido ou já encontrado na lista.')
+    }
+}
