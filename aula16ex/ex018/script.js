f78afb5f684e735cc21c437c5b40135615b6b0ad
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2023-06-18 20:55:58 -0300

    gg

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,21 +1,57 @@
 //alert('Olá')
-let num = window.document.querySelector('input#txtn1')
+let num = document.querySelector('input#fnun')
 let lista = window.document.querySelector('select#flista')
 let res = window.document.querySelector('div#res')
 let valores = []
 
 function isNumero(n) {
-
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
 }
 
 function inLista(n, l) {
-
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
 }
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valores ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor Inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+
     }
 }
