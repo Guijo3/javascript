6cc968fa2a9591f3f125f3e308102a8b9a5177c2
Guilherme José <88563923+Guijo3@users.noreply.github.com>
2023-06-18 21:10:52 -0300

    gggg

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,7 +1,7 @@
 //alert('Olá')
 let num = document.querySelector('input#fnun')
-let lista = window.document.querySelector('select#flista')
-let res = window.document.querySelector('div#res')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
 
 function isNumero(n) {
@@ -24,7 +24,7 @@ function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valores ${num.value} adicionado.`
+        item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
         res.innerHTML = ''
     } else {
@@ -41,17 +41,21 @@ function finalizar() {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores [0]
+        let soma = 0
+        let media = 0
         for(let pos in valores) {
+            soma += valores[pos]
             if (valores[pos] > maior)
             maior = valores[pos]
             if (valores[pos] < menor)
             menor = valores[pos]
         }
-
+        media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores dogitados é ${media}.</p>`
     }
 }
